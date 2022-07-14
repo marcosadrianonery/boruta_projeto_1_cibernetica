51333de1dc47eb843abce05c43bdee851831f59b
marcosadrianonery <marcosadrianonery@gmail.com>
2022-07-14 16:12:05 -0300

    Atualizando graficos

--- boruta_example copy.py
+++ boruta_example copy.py
@@ -1,20 +1,12 @@
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-import functionsBoruta
-import functions
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-import sklearn.model_selection as model_selection
-import numpy as np
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.model_selection import cross_validate, cross_val_predict
+from sklearn.model_selection import cross_validate
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import recall_score, accuracy_score, average_precision_score, mean_squared_error
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import average_precision_score, recall_score, make_scorer, precision_score, f1_score, roc_auc_score, accuracy_score, average_precision_score, mean_squared_error, classification_report, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
+import functionsBoruta
+import functions
+import numpy as np
 import matplotlib.pyplot as plt
 
 pastasName = ["Breast Cancer Wisconsin",
@@ -23,7 +15,7 @@ pastasName = ["Breast Cancer Wisconsin",
               "SaHeart", "Planning Relax",
               "SPECTF Heart", "Statlog (Heart)"]
 
-#pastasName = ["Planning Relax"]
+pastasName = ["Planning Relax"]
 
 
 saidaTeste = {}
@@ -33,7 +25,7 @@ for pasta in pastasName:
     ##############################################################################
     #   DATABASE PART
     ##############################################################################
-    print("-"*100)
+    print("#"*100)
     print("Dataset Name: ", pasta)
     print("-"*100)
 
@@ -60,11 +52,6 @@ for pasta in pastasName:
     #   CROSS VALIDATION PART
     ##############################################################################
 
-    model_normalizado = Pipeline(steps=[
-        ("normalizacao", MinMaxScaler()),
-        ("RFC", RandomForestClassifier(n_estimators=5))
-    ])
-
     model = RandomForestClassifier(n_estimators=5)
 
     metrics = {'precision': 'precision',
@@ -73,7 +60,7 @@ for pasta in pastasName:
                'f1': 'f1',
                'roc_auc': 'roc_auc',
                'AUPR': make_scorer(average_precision_score),
-               'neg_root_mean_squared_error': make_scorer(mean_squared_error)
+               'neg_root_mean_squared_error': make_scorer(mean_squared_error, squared=False)
                }
     scores = cross_validate(model, X_train_filtred,
                             Y_train_filtred, cv=10, scoring=metrics)
@@ -87,6 +74,30 @@ for pasta in pastasName:
     print('RMSE:', scores['test_neg_root_mean_squared_error'].mean())
     print("-"*100)
 
+    metricas_cross = [
+        scores['test_accuracy'].mean(),
+        scores['test_f1'].mean(),
+        scores['test_precision'].mean(),
+        scores['test_recall'].mean(),
+        scores['test_roc_auc'].mean(),
+        scores['test_AUPR'].mean(),
+        scores['test_neg_root_mean_squared_error'].mean()]
+    names = ["Acurácia", "F1",
+             "Precision", "Recall",
+             "ROC_AUC", "AUPR", "RMSE"]
+    fig, (ax1,  ax3) = plt.subplots(2)
+
+    width = 0.5
+    ax1.bar(names, metricas_cross, color='black',
+            width=width, edgecolor='white', label='Score')
+
+    ax1.set_ylabel('Valores')
+    ax1.set_title('CROSS VALIDATION')
+    # ax1.set_xticks(names)
+
+    # ax.set_xticklabels(metricas_predict)
+    ax1.legend()
+
     ##############################################################################
     #   TEST PART [Xtrain, Xtest, Ytrain, Ytest]
     ##############################################################################
@@ -94,9 +105,9 @@ for pasta in pastasName:
     X_treino, X_teste, y_treino, y_teste = train_test_split(
         X_train_filtred, Y_train_filtred, test_size=0.3, shuffle=True, random_state=32)
 
-    print("y_treino", y_treino)
-    print("y_teste", y_teste)
-    print("Y_train_filtred", Y_train_filtred)
+    #print("y_treino", y_treino)
+    #print("y_teste", y_teste)
+    #print("Y_train_filtred", Y_train_filtred)
 
     model_test = RandomForestClassifier(n_estimators=5)
 
@@ -112,10 +123,11 @@ for pasta in pastasName:
         precision_score(y_teste, y_predict),
         recall_score(y_teste, y_predict),
         roc_auc_score(y_teste, y_predict),
-        mean_squared_error(y_teste, y_predict)]
+        average_precision_score(y_teste, y_predict),
+        mean_squared_error(y_teste, y_predict, squared=False)]
     names = ["Acurácia", "F1",
              "Precision", "Recall",
-             "ROC_AUC", "RMSE"]
+             "ROC_AUC", "AUPR", "RMSE"]
 
     # print(relatorio)
     print("Acurácia de teste: ", metricas_predict[0])
@@ -123,29 +135,30 @@ for pasta in pastasName:
     print("precision_score de teste: ", metricas_predict[2])
     print("recall_score de teste: ", metricas_predict[3])
     print("roc_auc_score de teste: ", metricas_predict[4])
-    print("RMSE de teste: ", metricas_predict[5])
+    print("precision_recall de teste: ", metricas_predict[5]),
+    print("RMSE de teste: ", metricas_predict[6])
 
     print("-"*100)
 
     mat_conf = confusion_matrix(y_teste, y_predict, labels=model_test.classes_)
-    disp = ConfusionMatrixDisplay(
+    ax2 = ConfusionMatrixDisplay(
         confusion_matrix=mat_conf, display_labels=model_test.classes_)
     print("Matriz de confusão:")
     print(mat_conf)
-    disp.plot()
 
-    width = 0.15
-    fig, ax = plt.subplots()
+    #ax3 = plt.subplots()
 
-    plt.bar(names, metricas_predict, color='blue',
-            width=width, edgecolor='white', label='Median_Wealth')
+    ax3.bar(names, metricas_predict, color='red',
+            width=width, edgecolor='white', label='Score')
 
-    ax.set_ylabel('Valores')
-    ax.set_title('Agrupados')
-    ax.set_xticks(names)
+    ax3.set_ylabel('Valores')
+    ax3.set_title('Independente TEST')
+    ax3.set_xticks(names)
     # ax.set_xticklabels(metricas_predict)
-    ax.legend()
-    ax.plot()
+    ax3.legend()
+
+    plt.plot(ax=[ax1, ax2, ax3])
+    ax2.plot()
 
 plt.show()
-ax.show()
+# ax.show()
